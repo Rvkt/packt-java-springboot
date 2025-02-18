b4215b080c908d2c6c5366663ed6b899901ca8a7
Ravi Kant <46199672+Rvkt@users.noreply.github.com>
2025-02-18 06:45:47 +0530

    initial project upload

---
+++ src/main/java/io/rvkt/property_management/PropertyManagementApplication.java
@@ -0,0 +1,13 @@
+package io.rvkt.property_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyManagementApplication.class, args);
+	}
+
+}
